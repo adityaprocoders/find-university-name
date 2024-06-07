bdf46659316fdb801e85cb90eb4d613421ef7251
Aditya Kumar Singh <168503964+adityaprocoders@users.noreply.github.com>
2024-06-06 17:52:34 -1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const url = "http://universities.hipolabs.com/search?name=";
+const btn = document.querySelector("button");
+
+ 
+btn.addEventListener("click", async ()=> {
+    const country = document.querySelector("input").value;
+    const collegeArr = await getCollege(country);
+    showColl(collegeArr);
+});
+
+
+function showColl(collegeArr) {
+    const list = document.querySelector("#list");
+    list.innerHTML = "";
+    for(col of collegeArr){
+        const li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    };
+
+};
+
+async function getCollege(country) {
+   try{
+    const res = await axios.get(url + country);
+    return res.data;
+   }catch(e){
+     return e;
+   };
+};
+
+ 
